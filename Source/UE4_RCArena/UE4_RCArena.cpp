6e28e9e3fb969deeed51d4a8d709b65b7d34f5c3
Dan <dangeorgemarkdore@gmail.com>
2021-05-01 14:21:42 +0100

    Init
    Added Base Unreal Files

---
+++ Source/UE4_RCArena/UE4_RCArena.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UE4_RCArena.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE4_RCArena, "UE4_RCArena" );
---
+++ Source/UE4_RCArena/UE4_RCArenaGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE4_RCArenaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4_RCARENA_API AUE4_RCArenaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
